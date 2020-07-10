aaeb137239a36b12fe43e924f99c1fd14669c046
Nina Hofmann <nina.hofmann@computational.bio.uni-giessen.de>
2020-07-10 19:37:27 +0200

    Add function for analysis with 'compareCluster'. Allow different keytypes in input geneset for ORA and GSEA.

--- dge_pipeline/snakefiles/analyses/deseq2/lib/enrichment.R
+++ dge_pipeline/snakefiles/analyses/deseq2/lib/enrichment.R
@@ -1,13 +1,19 @@
 library(clusterProfiler)
 library(ReactomePA)
+library(org.Hs.eg.db)
 #library(stringr)
 
 # Over-representation analysis for a set of genes
-calc_ora <- function(gene, main = "", filename, out.dir = "ORA", GO = T, KEGG = T, REACTOME = F, ont = "BP", p.cut = 0.05, label.size = 12){
+calc_ora <- function(geneset, main = "", filename, out.dir = "ORA", GO = T, KEGG = T, REACTOME = F, ont = "BP", p.cut = 0.05, label.size = 12, 
+                     keytype = "SYMBOL"){
   if(!dir.exists(out.dir)){
     dir.create(out.dir)
   }
-  gene_bitr <- bitr(gene, fromType="SYMBOL", toType="ENTREZID", "org.Hs.eg.db")
+  if(keytype != "ENTREZID"){
+    gene_bitr <- bitr(geneset, fromType=keytype, toType="ENTREZID", "org.Hs.eg.db")
+  }else{
+    gene_bitr <- data.frame(ENTREZID=geneset)
+  }
   ora.list <- list()
   if(GO){
     for(o in ont){
@@ -43,24 +49,30 @@ calc_ora <- function(gene, main = "", filename, out.dir = "ORA", GO = T, KEGG = 
 }
 
 # Gene Set Enrichment Analysis
-calc_gsea <- function(res, name, ont = "BP", sort.by = "stat", KEGG = T, GO = T, REACTOME = F, nPerm = 1000, p.cut = 0.05, out.dir = "GSEA/"){
+calc_gsea <- function(geneset.df, filename, ont = "BP", sort.by = "stat", KEGG = T, GO = T, REACTOME = F, nPerm = 1000, p.cut = 0.05, 
+                      out.dir = "GSEA/", keytype = "SYMBOL", key.colname = "SYMBOL"){
   if(!dir.exists(out.dir)){
     dir.create(out.dir)
   }
   gsea.list <- list()
-  gene_bitr <- bitr(res$SYMBOL, fromType="SYMBOL", toType="ENTREZID", "org.Hs.eg.db")
-  res <- merge(res, gene_bitr, by = "SYMBOL")
-  res <- res[order(res[,sort.by], decreasing = TRUE),]
-  geneset_num <- as.numeric(res[,sort.by])
-  names(geneset_num) <- res$ENTREZID
+  if(keytype != "ENTREZID"){
+    gene_bitr <- bitr(geneset.df[,key.colname], fromType=keytype, toType="ENTREZID", "org.Hs.eg.db")
+    geneset.df <- merge(geneset.df, gene_bitr, by.x = key.colname, by.y = keytype)
+  }else{
+    colnames(geneset.df)[colnames(geneset.df)==key.colname] <- "ENTREZID"
+  }
+  geneset.df <- geneset.df[order(geneset.df[,sort.by], decreasing = TRUE),]
+  geneset_num <- as.numeric(geneset.df[,sort.by])
+  names(geneset_num) <- geneset.df$ENTREZID
+  
   if(KEGG){
     gsea_kegg <- try(gseKEGG(geneList = geneset_num, organism = "hsa", nPerm = nPerm, pvalueCutoff = p.cut, seed = T))
     if(nrow(data.frame(gsea_kegg)) > 0){
       plot_kegg <- dotplot(gsea_kegg, showCategory = 20, split = ".sign") + facet_grid(.~.sign) + theme(axis.title = element_text(size=18, face = "bold"), axis.text = element_text(size=15, face = "bold"), strip.text.x = element_text(size = 18, face = "bold"))
-      ggsave(paste(name,"_KEGG_dotplot.svg", sep = ""), device = "svg", plot = plot_kegg, path = paste(out.dir, sep = ""), width = 18, height = 15)
+      ggsave(paste(filename,"_KEGG_dotplot.svg", sep = ""), device = "svg", plot = plot_kegg, path = paste(out.dir, sep = ""), width = 18, height = 15)
       gsea_kegg.df <- data.frame(gsea_kegg)
       gsea_kegg.df$core_enrichment_SYMBOL <- sapply(gsea_kegg.df$core_enrichment, function(x){ x.split <- unlist(strsplit(x,"/")); return(paste(bitr(x.split, "ENTREZID", "SYMBOL", "org.Hs.eg.db")[["SYMBOL"]], collapse = "/"))})
-      write.table(gsea_kegg.df, file = paste(out.dir, "/", name, "_KEGG.csv", sep = ""), sep = ",", row.names = FALSE)
+      write.table(gsea_kegg.df, file = paste(out.dir, "/", filename, "_KEGG.csv", sep = ""), sep = ",", row.names = FALSE)
       gsea.list[["KEGG"]] <- gsea_kegg
     }
   }
@@ -69,10 +81,10 @@ calc_gsea <- function(res, name, ont = "BP", sort.by = "stat", KEGG = T, GO = T,
       gsea_go <- try(gseGO(geneset_num, ont = o, OrgDb = org.Hs.eg.db, keyType = "ENTREZID", nPerm = nPerm, pvalueCutoff = p.cut, seed = T))
       if(nrow(data.frame(gsea_go)) > 0){
         plot_go <- dotplot(gsea_go, showCategory = 20, split = ".sign") + facet_grid(.~.sign) + theme(axis.title = element_text(size=18, face = "bold"), axis.text = element_text(size=15, face = "bold"), strip.text.x = element_text(size = 18, face = "bold"))
-        ggsave(paste(name, "_GO_", o, "_dotplot.svg", sep = ""), device = "svg", plot = plot_go, path = paste(out.dir, sep = ""), width = 18, height = 15)
+        ggsave(paste(filename, "_GO_", o, "_dotplot.svg", sep = ""), device = "svg", plot = plot_go, path = paste(out.dir, sep = ""), width = 18, height = 15)
         gsea_go.df <- data.frame(gsea_go)
         gsea_go.df$core_enrichment_SYMBOL <- sapply(gsea_go.df$core_enrichment, function(x){ x.split <- unlist(strsplit(x,"/")); return(paste(bitr(x.split, "ENTREZID", "SYMBOL", "org.Hs.eg.db")[["SYMBOL"]], collapse = "/"))})
-        write.table(gsea_go.df, file = paste(out.dir, "/", name, "_GO_", o, ".csv", sep = ""), sep = ",", row.names = FALSE)
+        write.table(gsea_go.df, file = paste(out.dir, "/", filename, "_GO_", o, ".csv", sep = ""), sep = ",", row.names = FALSE)
         gsea.list[[o]] <- gsea_go
         # Problem: Bei gleichem value von by werden alle Pathways mit diesem Wert ausgegeben!
         # Loesung: by = pvalue?
@@ -84,12 +96,47 @@ calc_gsea <- function(res, name, ont = "BP", sort.by = "stat", KEGG = T, GO = T,
     gsea_reactome <- try(gsePathway(geneset_num, nPerm = nPerm, pvalueCutoff = p.cut, seed = T))
     if(nrow(data.frame(gsea_reactome)) > 0){
       plot_reactome <- dotplot(gsea_reactome, showCategory = 20, split = ".sign") + facet_grid(.~.sign) + theme(axis.title = element_text(size=18, face = "bold"), axis.text = element_text(size=15, face = "bold"), strip.text.x = element_text(size = 18, face = "bold"))
-      ggsave(paste(name,"_REACTOME_dotplot.svg", sep = ""), device = "svg", plot = plot_reactome, path = paste(out.dir, sep = ""), width = 18, height = 15)
+      ggsave(paste(filename,"_REACTOME_dotplot.svg", sep = ""), device = "svg", plot = plot_reactome, path = paste(out.dir, sep = ""), width = 18, height = 15)
       gsea_reactome.df <- data.frame(gsea_reactome)
       gsea_reactome.df$core_enrichment_SYMBOL <- sapply(gsea_reactome.df$core_enrichment, function(x){ x.split <- unlist(strsplit(x,"/")); return(paste(bitr(x.split, "ENTREZID", "SYMBOL", "org.Hs.eg.db")[["SYMBOL"]], collapse = "/"))})
-      write.table(gsea_reactome.df, file = paste(out.dir, "/", name, "_REACTOME.csv", sep = ""), sep = ",", row.names = FALSE)
+      write.table(gsea_reactome.df, file = paste(out.dir, "/", filename, "_REACTOME.csv", sep = ""), sep = ",", row.names = FALSE)
       gsea.list[["REACTOME"]] <- gsea_reactome
     }
   }
   return(gsea.list)
+}
+
+# Comparative overrepresentation analysis
+calc_compareCluster <- function(dataset, filename, out.dir = "ORA", GO = T, KEGG = T, REACTOME = F, ont = "BP", p.cut = 0.05, label.size = 12, 
+                                main = "", w = 18, h = 15){
+  if(!dir.exists(out.dir)){
+    dir.create(out.dir)
+  }
+  
+  if(GO){
+    for(o in ont){
+      cc_go <- try(compareCluster(dataset, fun = "enrichGO", keyType = "ENTREZID", OrgDb = "org.Hs.eg.db", ont = o, readable = T, pvalueCutoff = p.cut))
+      if(nrow(data.frame(cc_go)) > 0){
+        plot_go <- dotplot(cc_go, showCategory = 20, font.size = label.size) + ggtitle(main) #+ scale_y_discrete(labels=function(x)str_wrap(x, width = 20))
+        ggsave(paste(filename,"_",o,"_dotplot.svg", sep = ""), device = "svg", plot = plot_go, path = out.dir, width = w, height = h)
+        write.table(data.frame(cc_go), file = paste(out.dir, "/", filename, "_",o,".csv", sep = ""), sep = "\t", row.names = FALSE)
+      }
+    }
+  }
+  if(KEGG){
+    cc_kegg <- try(compareCluster(dataset, "enrichKEGG", organism = "hsa", use_internal_data = FALSE, pvalueCutoff = p.cut))
+    if(nrow(data.frame(cc_kegg)) > 0){
+      plot_kegg <- dotplot(cc_kegg, showCategory = 20, font.size = label.size) + ggtitle(main) #+ scale_y_discrete(labels=function(x)str_wrap(x, width = 20))
+      ggsave(paste(filename,"_KEGG_dotplot.svg", sep = ""), device = "svg", plot = plot_kegg, path = out.dir, width = w, height = h)
+      write.table(data.frame(cc_kegg), file = paste(out.dir, "/", filename, "_KEGG.csv", sep = ""), sep = "\t", row.names = FALSE)
+    }
+  }
+  if(REACTOME){
+    cc_reactome <- try(compareCluster(dataset, "enrichPathway", organism = "human", readable = T, pvalueCutoff = p.cut))
+    if(nrow(data.frame(cc_reactome)) > 0){
+      plot_reactome <- dotplot(cc_reactome, showCategory = 20, font.size = label.size) + ggtitle(main) #+ scale_y_discrete(labels=function(x)str_wrap(x, width = 20))
+      ggsave(paste(filename,"_REACTOME_dotplot.svg", sep = ""), device = "svg", plot = plot_reactome, path = out.dir, width = w, height = h)
+      write.table(data.frame(cc_reactome), file = paste(out.dir, "/", filename, "_REACTOME.csv", sep = ""), sep = "\t", row.names = FALSE)
+    }
+  }
 }
