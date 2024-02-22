54bbe7623b20bf6249de7fc0df13b131b6c9ee14
Nina Hofmann <nina.hofmann@computational.bio.uni-giessen.de>
2024-02-23 00:57:39 +0100

    Parse software versions from conda envs

---
+++ dge_pipeline/scripts/parse_software_versions.py
@@ -0,0 +1,16 @@
+import yaml
+import sys
+from pathlib import Path
+
+envs_dir = sys.argv[1]
+software_list = []
+yaml_list = Path(envs_dir).glob("*.yaml")
+file = open('multiqc_config.yaml', 'a')
+file.write('software_versions:\n')
+for conda_env in yaml_list:
+    conda = yaml.safe_load(conda_env.open('r'))
+    for entry in conda['dependencies']:
+        entry_split = entry.split("=")
+        tmp = str(entry_split[0]) + ': "' + str(entry_split[1] + '"')
+        file.write(f'  {tmp}\n')
+file.close()
